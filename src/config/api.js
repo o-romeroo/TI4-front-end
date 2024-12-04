a63a6d43ac38bb9c7c3d2d020a547f25df634e33
rmro-o <joao.sales.785111@sga.pucminas.br>
2024-12-03 23:30:26 -0300

    fix production api reference

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.NODE_ENV === 'production' ? 'https://massa-backend-e61621c35d2d.herokuapp.com/' : 'http://localhost:8000';
+const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'https://massa-backend-e61621c35d2d.herokuapp.com/';
 
 export default API_URL;
