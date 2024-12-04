d316af634e7e92b4da59165cd942d5a18d7e0ab6
rmro-o <joao.sales.785111@sga.pucminas.br>
2024-12-03 22:52:32 -0300

    change reference from dev to production environment

--- src/config/api.js
+++ src/config/api.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.NODE_ENV === 'production' ? '/api' : 'http://localhost:8000';
+const API_URL = process.env.NODE_ENV === 'production' ? 'https://massa-backend-e61621c35d2d.herokuapp.com/' : 'http://localhost:8000';
 
 export default API_URL;
