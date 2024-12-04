dca417a88fe7ca0b32e93b45ba8f9db6f52cca24
João Vitor Romero Sales <102482011+o-romeroo@users.noreply.github.com>
2024-12-03 22:32:41 -0300

    add initial files

---
+++ vite.config.js
@@ -0,0 +1,36 @@
+// import { fileURLToPath, URL } from 'node:url'
+
+// import { defineConfig } from 'vite'
+// import vue from '@vitejs/plugin-vue'
+
+// // https://vitejs.dev/config/
+// export default defineConfig({
+//   plugins: [
+//     vue(),
+//     // vueDevTools(),
+//   ],
+//   resolve: {
+//     alias: {
+//       '@': fileURLToPath(new URL('./src', import.meta.url))
+//     }
+//   }
+// })
+
+
+import { fileURLToPath, URL } from 'node:url';
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+import vuetify from 'vite-plugin-vuetify';
+
+
+export default defineConfig({
+  plugins: [
+    vue(),
+    vuetify()
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
+  },
+});
