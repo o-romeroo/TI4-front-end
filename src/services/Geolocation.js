8b8856476a1502afeb1477d0c68b6cf56f44ff38
João Vitor Romero Sales <102482011+o-romeroo@users.noreply.github.com>
2024-12-08 09:56:15 -0300

    Update Geolocation.js

--- src/services/Geolocation.js
+++ src/services/Geolocation.js
@@ -30,8 +30,14 @@ export async function getIpInfos() {
     const url = `https://ipinfo.io/${ip}?token=${token}`;
 
     try {
-        const response = await axios.get(url);
-        const data = response.data;
+        const response = await fetch(url, {
+            headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+            },
+            method: 'GET'
+        });
+        const data = await response.json();
         ipInfo.value = {
             country: getCountryName(data.country),
             city: data.city,
